1a56e4b78bdac770b65d48d453b5475d460ec1b2
Patrick Browne <pt.browne@gmail.com>
2018-01-28 17:33:13 +0100

    chore: add metadata parsing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,7 +11,8 @@ class BasicTestSuite(unittest.TestCase):
     def test_parse_xdi(self):
         df = xdi_pandas.parse('./tests/data/Agri1_dig_sol_3refs_sc1.lcf')
         assert df.shape == (237, 7)
-        assert list(df.columns) == [
+        eq = lambda l: l[0] == l[1]
+        assert all(map(eq, zip(df.columns, [
             'wavelength inverse Angstrom',
             'data',
             'fit',
@@ -19,7 +20,29 @@ class BasicTestSuite(unittest.TestCase):
             'AZP',
             'Zn-Methionine',
             'nano-3_(A-2016)'
-        ]
+        ])))
+
+    def test_metadata(self):
+        metadata = xdi_pandas.parse_metadata('./tests/data/Agri1_dig_sol_3refs_sc1.lcf')
+
+        assert metadata['Column']['1'] == 'wavelength inverse Angstrom'
+        assert metadata['Column']['2'] == 'data'
+        assert metadata['Column']['3'] == 'fit'
+        assert metadata['Column']['4'] == 'residual'
+        assert metadata['Column']['5'] == 'AZP'
+        assert metadata['Column']['6'] == 'Zn-Methionine'
+        assert metadata['Column']['7'] == 'nano-3_(A-2016)'
+
+        assert metadata['Element']['edge'] == 'K'
+        assert metadata['Element']['symbol'] == 'Zn'
+        assert metadata['Athena']['e0'] ==  9663.5
+        assert metadata['Athena']['eshift'] ==  0
+        assert metadata['Athena']['rbkg'] ==  1.0
+        assert metadata['Athena']['importance'] ==  1
+        assert metadata['Athena']['standard'] ==  None
+        assert metadata['Athena']['bkg_kweight'] ==  3
+        assert metadata['Athena']['edge_step'] ==  1.0008537
+        assert metadata['Athena']['fixed_step'] ==  False
 
 if __name__ == '__main__':
     unittest.main()
---
+++ xdi_pandas/core.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from . import helpers
+import os
+import pandas as pd
+import re
+import json
+
+metadata_sep = re.compile(':\s+')
+
+
+def set_dict_path(d, path, value):
+    cur = d
+    splitted = path.split('.')
+    l = len(splitted)
+    for i, t in enumerate(splitted):
+        if i != l - 1:
+            cur[t] = cur.get(t, {})
+        else:
+            cur[t] = value
+        cur = cur[t]
+
+def parse_metadata_value(v):
+    try:
+        return float(v)
+    except ValueError as e:
+        pass
+    if v == 'None':
+        return None
+    elif v == 'no':
+        return False
+    elif v == 'yes':
+        return True
+    else:
+        return v
+
+
+def parse_metadata(filename):
+    metadata = {}
+    with open(filename) as f:
+        for l in f.readlines():
+            if l.startswith('#'):
+                if l.startswith('# ///'):
+                    break
+
+                splitted = re.split(metadata_sep, l, maxsplit=2)
+                if len(splitted) < 2:
+                    continue
+                path, value = splitted
+                path = path[2:]
+                value = parse_metadata_value(value.strip())
+                set_dict_path(metadata, path, value)
+    return metadata
+
+
+def parse(filename):
+    metadata = parse_metadata(filename)
+    columns = [
+        metadata['Column'][str(k)]
+        for k in sorted(map(int, metadata['Column'].keys()))
+    ]
+
+    data = pd.read_csv(
+        filename,
+        comment='#',
+        sep='\s+',
+        header=None,
+        names=columns)
+    return data
